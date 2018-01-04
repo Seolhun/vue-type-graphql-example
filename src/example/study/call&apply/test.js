1308f5db2632eb32ba25c763231c065b3bc4c60e
Hun, Seol(Sol) <shun10114@gmail.com>
2018-01-05 00:19:11 +0900

    Added bind examples (#11)
    * Testing is completed to install from npm repo on the project
    
    * Arragned files
    
    * Added call&Apply examples
    
    * Added call&Apply examples

--- src/example/study/call&apply/test.js
+++ src/example/study/call&apply/test.js
@@ -2,29 +2,69 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var CallAndApply = /** @class */ (function () {
     function CallAndApply() {
+        this.numbers = [6, 7, 8, 9, 10];
     }
     CallAndApply.prototype.call = function (param1) {
-        console.log.apply(console, ['call param1 : '].concat(param1));
+        console.log('CallAndApply call param1 : ', param1);
     };
-    CallAndApply.prototype.apply = function (param1, param2) {
-        console.log('apply param1 : ', param1);
-        console.log('apply param2 : ', param2);
-    };
-    CallAndApply.prototype.apply2 = function () {
+    CallAndApply.prototype.apply = function () {
         var param1 = [];
         for (var _i = 0; _i < arguments.length; _i++) {
             param1[_i] = arguments[_i];
         }
-        console.log('apply2 param1 : ', param1);
+        console.log('CallAndApply apply param1 : ', param1);
+    };
+    CallAndApply.prototype.apply2 = function (param1, param2) {
+        console.log('CallAndApply apply2 param1 : ', param1);
+        console.log('CallAndApply apply param2 : ', param2);
+    };
+    CallAndApply.prototype.bind = function (param1) {
+        console.log('CallAndApply bind pram1 : ', param1);
     };
     CallAndApply.prototype.run = function () {
-        var numbers = [5, 6, 2, 3, 7];
-        this.call.call(null, numbers);
+        this.call.call(null, this.numbers);
         console.log('----------------------------');
-        this.apply.apply(null, numbers);
+        this.apply.apply(null, this.numbers);
         console.log('----------------------------');
-        this.apply2.apply(null, numbers);
+        this.apply2.apply(null, this.numbers);
+        console.log('----------------------------');
+        this.bind.bind(null, this.numbers).call();
+        this.bind.bind(null, this.numbers).apply();
+        console.log('======================================================');
+        console.log('======================================================');
+        var ca2 = new CallAndApply2();
+        this.call.call(null, ca2.numbers);
+        console.log('----------------------------');
+        this.apply.apply(null, ca2.numbers);
+        console.log('----------------------------');
+        this.apply2.apply(null, ca2.numbers);
+        console.log('----------------------------');
+        this.bind.bind(null, ca2.numbers).call();
+        this.bind.bind(null, ca2.numbers).apply();
     };
     return CallAndApply;
+}());
+var CallAndApply2 = /** @class */ (function () {
+    function CallAndApply2() {
+        this.numbers = [1, 2, 3, 4, 5];
+    }
+    CallAndApply2.prototype.call = function (param1) {
+        console.log('CallAndApply2 call param1 : ', param1);
+    };
+    CallAndApply2.prototype.apply = function () {
+        var param1 = [];
+        for (var _i = 0; _i < arguments.length; _i++) {
+            param1[_i] = arguments[_i];
+        }
+        console.log('CallAndApply2 apply param1 : ', param1);
+    };
+    CallAndApply2.prototype.apply2 = function (param1, param2) {
+        console.log('CallAndApply2 apply2 param1 : ', param1);
+        console.log('CallAndApply2 apply param2 : ', param2);
+    };
+    CallAndApply2.prototype.bind = function (param1) {
+        console.log('CallAndApply2 bind pram1 : ', param1);
+    };
+    return CallAndApply2;
 }());
 exports.default = CallAndApply;
--- src/example/study/call&apply/test.ts
+++ src/example/study/call&apply/test.ts
@@ -1,24 +1,74 @@
 class CallAndApply {
+  numbers: number[];
+
+  constructor() {
+    this.numbers = [6, 7, 8, 9, 10];
+  }
+
   call(param1) {
-    console.log('call param1 : ', ...param1);
+    console.log('CallAndApply call param1 : ', param1);
   }
 
-  apply(param1, param2) {
-    console.log('apply param1 : ', param1);
-    console.log('apply param2 : ', param2);
+  apply(...param1) {
+    console.log('CallAndApply apply param1 : ', param1);
   }
 
-  apply2(...param1) {
-    console.log('apply2 param1 : ', param1);
+  apply2(param1, param2) {
+    console.log('CallAndApply apply2 param1 : ', param1);
+    console.log('CallAndApply apply param2 : ', param2);
+  }
+
+  bind(param1) {
+    console.log('CallAndApply bind pram1 : ', param1);
   }
 
   run() {
-    const numbers = [5, 6, 2, 3, 7];
-    this.call.call(null, numbers);
+    this.call.call(null, this.numbers);
     console.log('----------------------------');
-    this.apply.apply(null, numbers);
+    this.apply.apply(null, this.numbers);
     console.log('----------------------------');
-    this.apply2.apply(null, numbers);
+    this.apply2.apply(null, this.numbers);
+    console.log('----------------------------');
+    this.bind.bind(null, this.numbers).call();
+    this.bind.bind(null, this.numbers).apply();
+
+    console.log('======================================================');
+    console.log('======================================================');
+
+    const ca2 = new CallAndApply2();
+    this.call.call(null, ca2.numbers);
+    console.log('----------------------------');
+    this.apply.apply(null, ca2.numbers);
+    console.log('----------------------------');
+    this.apply2.apply(null, ca2.numbers);
+    console.log('----------------------------');
+    this.bind.bind(null, ca2.numbers).call();
+    this.bind.bind(null, ca2.numbers).apply();
+  }
+}
+
+class CallAndApply2 {
+  numbers: number[];
+
+  constructor() {
+    this.numbers = [1, 2, 3, 4, 5];
+  }
+
+  call(param1) {
+    console.log('CallAndApply2 call param1 : ', param1);
+  }
+
+  apply(...param1) {
+    console.log('CallAndApply2 apply param1 : ', param1);
+  }
+
+  apply2(param1, param2) {
+    console.log('CallAndApply2 apply2 param1 : ', param1);
+    console.log('CallAndApply2 apply param2 : ', param2);
+  }
+
+  bind(param1) {
+    console.log('CallAndApply2 bind pram1 : ', param1);
   }
 }
 
