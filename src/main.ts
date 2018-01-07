80426d73d43d576caecb7053f50a5820804d91fa
Hun, Seol(Sol) <shun10114@gmail.com>
2018-01-07 23:00:08 +0900

    Feature/callback (#12)
    * Testing is completed to install from npm repo on the project
    
    * Arragned files
    
    * Added call&Apply examples
    
    * Added call&Apply examples
    
    * Spread example and readme

--- src/main.ts
+++ src/main.ts
@@ -3,3 +3,7 @@ console.log('main.ts');
 import CallAndApply from './example/study/call&apply/test';
 const callAndApply = new CallAndApply();
 callAndApply.run();
+
+import Spread from './example/study/spread/test';
+const spread = new Spread();
+spread.run();
