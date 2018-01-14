393a90973f6464c2162cd9f38b36d962958b12a9
SeolHun <shun10114@gmail.com>
2018-01-14 22:55:57 +0900

    Is Added Simple graphql examples

--- server/src/schema/schema.ts
+++ server/src/schema/schema.ts
@@ -8,12 +8,22 @@ const {
   GraphQLSchema,
 } = graphql;
 
+const CompanyType = new GraphQLObjectType({
+  name: 'Company',
+  fields: {
+    id: { type: graphql.GraphQLString },
+    name: { type: graphql.GraphQLString },
+    description: { type: graphql.GraphQLString },
+  },
+});
+
 const UserType = new GraphQLObjectType({
   name: 'User',
   fields: {
     id: { type: graphql.GraphQLString },
     name: { type: graphql.GraphQLString },
     age: { type: graphql.GraphQLInt },
+    company: { type: CompanyType },
   },
 });
 
@@ -24,10 +34,21 @@ const RootQuery = new GraphQLObjectType({
       type: UserType,
       args: {
         id: { type: graphql.GraphQLString },
-        name: { type: graphql.GraphQLString },
       },
       resolve(parentValue, args) {
+        console.log('UserType', parentValue, args);
         return axios.get(`http://localhost:3000/users/${args.id}`)
+          .then((response) => response.data);
+      },
+    },
+    company: {
+      type: CompanyType,
+      args: {
+        id: { type: graphql.GraphQLString },
+      },
+      resolve(parentValue, args) {
+        console.log('CompanyType', parentValue, args);
+        return axios.get(`http://localhost:3000/companies/${args.id}`)
           .then((response) => response.data);
       },
     },
