67ae30baa1dfcf74ea567469844615940cde1e8d
Seolhun <shun10114@gmail.com>
2018-04-02 20:43:55 +0900

    [Server] - Updated Config setting method

--- server/src/config/environments/index.ts
+++ server/src/config/environments/index.ts
@@ -2,8 +2,27 @@ import DEV from './develop';
 import PROD from './production';
 import TEST from './test';
 
+interface SettingEnvironments {
+  NODE_ENV: string;
+  EXPRESS_PORT: number;
+
+  // Session Key
+  COOKIE_SESSION_MAX_AGE: number;
+  COOKIE_SESSION_KEYS: string[];
+  SESSION_MAX_AGE: number;
+  SESSION_KEYS: string[];
+
+  // Oauth2
+  FACEBOOK_CLIENT_ID?: string;
+  FACEBOOK_CLIENT_SECRET_ID?: string;
+  GITHUB_CLIENT_ID?: string;
+  GITHUB_CLIENT_SECRET_ID?: string;
+  GOOGLE_CLIENT_ID?: string;
+  GOOGLE_CLIENT_SECRET_ID?: string;
+}
+
 class Config {
-  static setConfiguration() {
+  static setConfiguration(): SettingEnvironments  {
     let env;
     if (process.env.NODE_ENV === 'producton') {
       process.env.NODE_ENV = DEV.NODE_ENV;
@@ -22,4 +41,4 @@ class Config {
   }
 }
 
-export { Config };
+export { Config, SettingEnvironments };
--- server/src/config/environments/test.ts
+++ server/src/config/environments/test.ts
@@ -1,8 +1,20 @@
+import { SettingEnvironments } from './';
+
 export default {
   NODE_ENV: 'test',
   EXPRESS_PORT: 4000,
 
+  // Session Key
+  COOKIE_SESSION_MAX_AGE: 1000 * 60 * 60 * 24 * 30,
+  COOKIE_SESSION_KEYS: ['seolhun-hicord-examples'],
+  SESSION_MAX_AGE: 1000 * 60 * 30,
+  SESSION_KEYS: ['seolhun-hicord-examples'],
+
   // Oauth2
+  FACEBOOK_CLIENT_ID: '',
+  FACEBOOK_CLIENT_SECRET_ID: '',
   GITHUB_CLIENT_ID: '',
   GITHUB_CLIENT_SECRET_ID: '',
-};
+  GOOGLE_CLIENT_ID: '',
+  GOOGLE_CLIENT_SECRET_ID: '',
+} as SettingEnvironments;
