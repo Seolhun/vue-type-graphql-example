baaec08fe73b8bdc7bfe2a531324b4b42fbe01da
Seolhun <shun10114@gmail.com>
2019-09-03 02:15:35 +0900

    Update docker compose with db

---
+++ server/db/Dockerfile
@@ -0,0 +1,3 @@
+FROM mariadb/server:10.3
+
+ADD default.sql /docker-entrypoint-initdb.d
--- server/src/config/database/index.ts
+++ server/src/config/database/index.ts
@@ -5,49 +5,60 @@ import {
   UpdateOptions,
   DestroyOptions,
 } from "sequelize";
+require("dotenv").config();
 
 import { logger } from "../logger";
 import { initDefaultData } from "./default";
 
-const sequelize = new Sequelize("shooney_management", "dev", "dev", {
-  host: "localhost",
-  dialect: "mariadb",
-  port: 3306,
-  timezone: "Etc/GMT0",
-  pool: {
-    max: 10,
-    min: 0,
-    acquire: 30000,
-    idle: 20000,
+const sequelize = new Sequelize(
+  process.env.DB_DATABASE || "",
+  process.env.DB_USER || "",
+  process.env.DB_ROOT_PASSWORD || "",
+  {
+    host: process.env.DB_HOST || "127.0.0.1",
+    port: Number.parseInt(process.env.DB_PORT || "3306", 10),
+    dialect: "mariadb",
+    dialectOptions: {
+      connectTimeout: 1000,
+      timezone: "Etc/GMT0",
+    },
+    pool: {
+      max: 10,
+      min: 1,
+      acquire: 30000,
+      idle: 20000,
+    },
+    define: {
+      charset: "utf8",
+      underscored: true,
+      timestamps: true,
+      createdAt: "created_at",
+      updatedAt: "updated_at",
+      deletedAt: "deleted_at",
+      paranoid: true,
+    },
+    logging: false,
+    benchmark: false,
+    hooks: {
+      beforeFind: (options: FindOptions) => {
+        logger.debug(`beforeFind`);
+        logger.debug(options);
+      },
+      beforeCreate: (instance: any, options: CreateOptions) => {
+        logger.debug(`beforeCreate`);
+        logger.debug(instance);
+      },
+      beforeUpdate: (instance: any, options: UpdateOptions) => {
+        logger.debug(`beforeUpdate`);
+        logger.debug(instance);
+      },
+      beforeDestroy: (instance: any, options: DestroyOptions) => {
+        logger.debug(`beforeDestroy`);
+        logger.debug(instance);
+      },
+    },
   },
-  define: {
-    charset: "utf8",
-    underscored: true,
-    timestamps: true,
-    createdAt: "created_at",
-    updatedAt: "updated_at",
-    deletedAt: "deleted_at",
-    paranoid: true,
-  },
-  hooks: {
-    beforeFind: (options: FindOptions) => {
-      logger.debug(`beforeFind`);
-      logger.debug(options);
-    },
-    beforeCreate: (instance: any, options: CreateOptions) => {
-      logger.debug(`beforeCreate`);
-      logger.debug(instance);
-    },
-    beforeUpdate: (instance: any, options: UpdateOptions) => {
-      logger.debug(`beforeUpdate`);
-      logger.debug(instance);
-    },
-    beforeDestroy: (instance: any, options: DestroyOptions) => {
-      logger.debug(`beforeDestroy`);
-      logger.debug(instance);
-    },
-  },
-});
+);
 
 sequelize.sync({ force: true }).then(() => {
   initDefaultData();
